e6801b317c3af8fb0973b3fa84536ffabdf15db3
DangMinhHuy308 <huyminhdang5@gmail.com>
2024-03-23 19:51:03 +0700

    API Board Detalis

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -3,18 +3,40 @@
 
 import ApiError from '~/utils/ApiError'
 import { slugify } from '~/utils/formatter'
+import { boardModel } from '~/models/boardModel'
+import { StatusCodes } from 'http-status-codes'
 const createNew = async (reqBody) => {
   try {
     const newBoard = {
       ...reqBody,
       slug: slugify(reqBody.title)
     }
+    const createdBoard = await boardModel.createNew(newBoard)
+    console.log(createdBoard)
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
+
     // Trả kq về trong service luôn phải có return
-    return newBoard
+    return getNewBoard
+  } catch (error) {
+    throw error
+  }
+}
+const getDetails = async (boardId) => {
+  try {
+
+    const board = await boardModel.getDetails(boardId)
+    if (!board) {
+      throw new ApiError(StatusCodes.NOT_FOUND, 'Board not found')
+
+    }
+
+    // Trả kq về trong service luôn phải có return
+    return board
   } catch (error) {
     throw error
   }
 }
 export const boardService ={
-  createNew
+  createNew,
+  getDetails
 }
