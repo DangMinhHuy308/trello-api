90ba023d3ad4eab95ac2a4a4d42514f50447954d
DangMinhHuy308 <huyminhdang5@gmail.com>
2024-03-26 16:05:24 +0700

    Update Whitelist Domains

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,7 @@
  */
 // những domain dc phép truy cập tới tài nguyên của server
 export const WHITELIST_DOMAINS = [
-  
+  'https://trello-web-smoky-iota.vercel.app/'
 ]
 export const BOARD_TYPES = {
   PUBLIC:'public',
