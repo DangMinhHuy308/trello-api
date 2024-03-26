40a10455283ab190e754efa165a48190719de36c
DangMinhHuy308 <huyminhdang5@gmail.com>
2024-03-26 14:56:58 +0700

    Deploy p3

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,7 @@
  */
 // những domain dc phép truy cập tới tài nguyên của server
 export const WHITELIST_DOMAINS = [
-  'http://localhost:5173'
+  
 ]
 export const BOARD_TYPES = {
   PUBLIC:'public',
